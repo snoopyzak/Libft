74dbdf6150416056287463fa23d6496ae77ddba1
snoopy <snoopy@archlinux.org>
2024-08-19 23:04:45 +0100

    header!

---
+++ libft.h
@@ -0,0 +1,15 @@
+#ifndef LIBFT_H
+
+# define LIBFT_H
+
+int 	ft_atoi(char *str);
+void    ft_putchar(char c);
+void    ft_putstr(char  *str);
+char    *ft_strcat(char *dest, char *str);
+char    *strchr(const char *s, int c);
+char    *ft_strcpy(char *dest, char *src);
+char    *ft_strdup(char *str);
+int 	ft_strlen(char  *str);
+char	*ft_strncpy(char *dest, char *src, int n);
+
+#endif
