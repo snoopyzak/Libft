8f64ecf8097755a3819c518080abdffd9a252026
snoopy <snoopy@archlinux.org>
2024-08-20 01:16:14 +0100

    moremoremore

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,3 +1,19 @@
 #include <string.h>
 
-size_t	strlcpy(char *dst, const char *src, size_t size);
+size_t	strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t src_len = strlen(src);
+
+	if (size > 0)
+	{
+		size_t i = 0;
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+
+	return (src_len);
+}
--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,15 +1,15 @@
 char	*ft_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    i = 0;
-    if (i == n)
-        return (0);
-    while (src[i] != 0 && i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = 0;
-    return (dest);
+	i = 0;
+	if (i == n)
+		return (0);
+	while (src[i] != 0 && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
 }
