aadb8a07fddd43f8b0af00bbf05f44f4fd54d186
snoopy <snoopy@archlinux.org>
2024-08-20 01:54:50 +0100

    good!

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,9 +14,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	while (dst[i] != 0)
 		i++;
 	dst_len = i;
+	if (size <= dst_len)
+		return (size + src_len);
 	i = 0;
 	while (src[i] != 0 && i < size - dst_len - 1)
-		dst[dst_len++] = src[i];
-	dst[dst_len] = 0;
-	return (dst);
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = 0;
+	return (src_len + dst_len);
 }
