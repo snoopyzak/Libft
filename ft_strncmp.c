20c303230391beff25a3507d1f403b7ff62f7173
snoopy <snoopy@archlinux.org>
2024-08-21 15:05:34 +0100

    yes

---
+++ ft_strncmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+size_t	ft_strncmp(char *s1, char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
+		i++;
+	if (i == n)
+		return (0);
+	return (s1[i] - s2[i]);
+}
