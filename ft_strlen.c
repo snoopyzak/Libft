2caf60f2cbcab47338b046017419f889df29c681
Zakaria Akrab <zaakrab@c3r9p7.1337.ma>
2024-10-23 13:26:39 +0100

    strlen fixed xd

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,9 +1,21 @@
+#include <stddef.h>
+
 int ft_strlen(char  *str)
 {
     int i;
 
     i = 0;
+    if (str == NULL)
+	return (0);
     while (str[i] != 0)
         i++;
     return (i);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char *s = NULL;
+	printf("%u", ft_strlen(s));
+}*/
